c2c76e22361f1d880361799fc1e7e2b65286fb02
Vivek Kumar Maskara <maskaravivek@gmail.com>
2024-05-12 23:45:56 -0700

    Minor changes

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -2,7 +2,7 @@ package com.acme;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Welcome to the sample app.";
     }
 
     public static void main(String[] args) {
